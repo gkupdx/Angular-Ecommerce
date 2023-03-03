5ef8ab2880a1b5d36e575368c0117f3bcf4569a4
Grady Ku <gkupdx@gmail.com>
2023-03-03 13:47:57 -0800

    Init Firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+/* Firebase config imports */
+import { firebaseConfig } from 'firebase.config';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'k-ecommerce';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    initializeApp(firebaseConfig);
+  }
 }

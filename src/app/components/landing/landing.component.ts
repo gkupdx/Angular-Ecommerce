e61d787c70f9473439e55abdf634599235c00ccc
Grady Ku <gkupdx@gmail.com>
2023-03-02 21:45:40 -0800

    Import new animations

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { LoginForm } from 'src/app/interfaces/Forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { fadeInSlow, slideDownSlow } from 'src/app/utilities/animations';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
+  animations: [
+    fadeInSlow,
+    slideDownSlow,
+  ],
 })
 export class LandingComponent {
   form: LoginForm = {

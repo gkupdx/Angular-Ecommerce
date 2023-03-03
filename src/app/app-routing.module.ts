3e9a78508a9296c119997312561f73e145dbcba1
Grady Ku <gkupdx@gmail.com>
2023-03-03 13:36:19 -0800

    Add new route to store component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { RegisterComponent } from './components/register/register.component';
+import { StoreComponent } from './components/store/store.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'store', component: StoreComponent },
 ];
 
 @NgModule({

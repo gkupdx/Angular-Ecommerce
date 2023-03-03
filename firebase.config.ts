61345836911c3a68cdc2c7c1b3d6b40ff2b28da2
Grady Ku <gkupdx@gmail.com>
2023-03-02 20:26:13 -0800

    Setup Firebase hosting

---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkGzItuKGx_XJBZMfRM6NUt7XMeiKeLU4",
+  authDomain: "angular-ecommerce-22bbe.firebaseapp.com",
+  projectId: "angular-ecommerce-22bbe",
+  storageBucket: "angular-ecommerce-22bbe.appspot.com",
+  messagingSenderId: "306952987659",
+  appId: "1:306952987659:web:235a0f120a9f8b5018930e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

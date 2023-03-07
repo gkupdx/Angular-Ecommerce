4a4c0e1ea7a5eb9c72dbe792ae6ec9ce433fe06d
Grady Ku <gkupdx@gmail.com>
2023-03-06 16:35:37 -0800

    Add new route to account component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { RegisterComponent } from './components/register/register.component';
 import { StoreComponent } from './components/store/store.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'store', component: StoreComponent },
+  { path: 'account', component: AccountComponent },
 ];
 
 @NgModule({

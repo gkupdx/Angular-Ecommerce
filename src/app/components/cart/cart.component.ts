b3f0117387fc9b1622b64d3d43c75f006a59a9c0
Grady Ku <gkupdx@gmail.com>
2023-03-09 14:00:19 -0800

    Add new cart component

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent {
+  constructor(private cartService: CartService) {}
+
+  getCartInfo() {
+    return this.cartService.getCart();
+  }
+
+  getCartTotal() {
+    return this.cartService.calculateTotal();
+  }
+}

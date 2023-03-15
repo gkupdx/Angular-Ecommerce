bd1c2e2af8a04091a4c8dc34e6c3cc3b7b4fdbf0
Grady Ku <gkupdx@gmail.com>
2023-03-14 23:36:00 -0700

    Import animation

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { slideIn } from 'src/app/utilities/animations';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  animations: [
+    slideIn,
+  ]
 })
 export class CartComponent {
   constructor(private cartService: CartService) {}

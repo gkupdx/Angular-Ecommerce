26243aabb7a8f6c4ee0595cdc412e4763cdf33ee
Grady Ku <gkupdx@gmail.com>
2023-03-15 14:05:38 -0700

    Remove product loading (moved to separate components)

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Product, NewRelease } from 'src/app/interfaces/Product';
-import { ProductService } from 'src/app/services/product.service';
 import { fadeInSlow, slideDownSlow } from 'src/app/utilities/animations';
 
 @Component({
@@ -13,17 +11,6 @@ import { fadeInSlow, slideDownSlow } from 'src/app/utilities/animations';
   ],
 })
 export class StoreComponent {
-  storeItems: Product[] = [];
-  newItem: NewRelease = {
-    name: '',
-    price: '',
-    imgSrc: '',
-    premiere: '',
-  };
 
-  // want to load up store with products on component creation
-  constructor(private productService: ProductService) {
-    // this.storeItems = this.productService.getProducts();
-    this.newItem = this.productService.getNewRelease();
-  }
+  constructor() {}
 }

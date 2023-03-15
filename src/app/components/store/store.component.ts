8e30b4dba457fdeb1df773cfef4bf3a49a5c216e
Grady Ku <gkupdx@gmail.com>
2023-03-14 23:40:09 -0700

    Import animations; get new release product

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
-import { Product } from 'src/app/interfaces/Product';
+import { Product, NewRelease } from 'src/app/interfaces/Product';
 import { ProductService } from 'src/app/services/product.service';
-import { slideIn } from 'src/app/utilities/animations';
+import { fadeInSlow, slideDownSlow } from 'src/app/utilities/animations';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.css'], 
   animations: [
-    slideIn,
+    fadeInSlow,
+    slideDownSlow,
   ],
 })
 export class StoreComponent {
   storeItems: Product[] = [];
+  newItem: NewRelease = {
+    name: '',
+    price: '',
+    imgSrc: '',
+    premiere: '',
+  };
 
   // want to load up store with products on component creation
   constructor(private productService: ProductService) {
-    this.storeItems = this.productService.getProducts();
+    // this.storeItems = this.productService.getProducts();
+    this.newItem = this.productService.getNewRelease();
   }
 }

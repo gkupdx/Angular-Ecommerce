a16e8346b13da25d3fa8de4c0fe87db0b2485c2e
Grady Ku <gkupdx@gmail.com>
2023-03-15 21:53:37 -0700

    Remove unnecessary code

--- src/app/components/product/topseller/topseller.component.ts
+++ src/app/components/product/topseller/topseller.component.ts
@@ -1,8 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
 import { Product } from 'src/app/interfaces/Product';
-import { CartProduct } from 'src/app/interfaces/Product';
-import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 import { fadeInSlow } from 'src/app/utilities/animations';
 
@@ -15,40 +12,9 @@ import { fadeInSlow } from 'src/app/utilities/animations';
   ]
 })
 export class TopsellerComponent {
-  @Input() product: Product = {} as Product;
   topProducts: Product[] = [];
-  cartProduct: CartProduct = {
-    name: '',
-    price: '',
-    imgSrc: '',
-    count: 0,
-  }
-  itemCount: number = 0;
-  isAddedToCart: boolean = false;
-  minusIcon = faMinus;
-  plusIcon = faPlus;
 
-  constructor(private cartService: CartService, private productService: ProductService) {
+  constructor(private productService: ProductService) {
     this.topProducts = this.productService.getTopProducts();
-  }
-
-  emitAddToCart() {
-    this.cartProduct.name = this.product.name;
-    this.cartProduct.price = this.product.price;
-    this.cartProduct.imgSrc = this.product.imgSrc;
-    this.cartProduct.count = this.itemCount;
-
-    this.isAddedToCart = true;
-
-    this.cartService.addToCart(this.cartProduct);
-  }
-
-  decreaseCount() {
-    if (this.itemCount === 0) return;
-    this.itemCount -= 1;
-  }
-
-  increaseCount() {
-    this.itemCount += 1;
   }
 }

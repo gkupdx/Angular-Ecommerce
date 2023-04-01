e539c9d2b315923aa1fe03d822e37eb19d2c353f
Grady Ku <gkupdx@gmail.com>
2023-04-01 12:21:36 -0700

    Add new animation, icon and password update functionality

--- src/app/components/account/passwordchange/passwordchange.component.ts
+++ src/app/components/account/passwordchange/passwordchange.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { AuthService } from 'src/app/services/auth.service';
 import { UpdatePassForm } from 'src/app/interfaces/Forms';
-import { slideIn } from 'src/app/utilities/animations';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { slideIn, slideDown } from 'src/app/utilities/animations';
+import { faArrowLeft, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-passwordchange',
@@ -10,17 +11,23 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./passwordchange.component.css'],
   animations: [
     slideIn,
+    slideDown,
   ]
 })
 export class PasswordchangeComponent {
   form: UpdatePassForm = {
-    currPassword: '',
     newPassword: '',
     passConfirm: '',
   }
+  updateSuccess: boolean = false;
   backArrow = faArrowLeft;
+  checkmark = faCheck;
 
-  constructor(private location: Location) {}
+  constructor(private location: Location, private authService: AuthService) {}
+
+  pwdChangeSubmit() {
+    this.updateSuccess = this.authService.updatePassword(this.form);
+  }
 
   goToPrev() {
     this.location.back();

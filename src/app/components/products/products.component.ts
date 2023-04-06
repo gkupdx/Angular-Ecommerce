43b2ebc8ebe077242d3c57d64322cb9eb059cba4
Grady Ku <gkupdx@gmail.com>
2023-04-05 20:18:55 -0700

    Add sorting feature for products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Product } from 'src/app/interfaces/Product';
 import { ProductService } from 'src/app/services/product.service';
 import { fadeInSlow } from 'src/app/utilities/animations';
@@ -11,10 +11,19 @@ import { fadeInSlow } from 'src/app/utilities/animations';
     fadeInSlow,
   ]
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnChanges {
+  @Input() filterPrice: string;
   products: Product[] = [];
 
   constructor(private productService: ProductService) {
     this.products = this.productService.getProducts();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['filterPrice'].currentValue === 'Low -> Hi') {
+      this.products = this.productService.getSortedProducts('L2H');
+    } else if (changes['filterPrice'].currentValue === 'Hi -> Low') {
+      this.products = this.productService.getSortedProducts('H2L');
+    }
   }
 }

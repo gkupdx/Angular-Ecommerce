98a916cb07328c1f121d96ecca6d383682708b57
Grady Ku <gkupdx@gmail.com>
2023-04-15 17:12:04 -0700

    Add functionality for add to cart button

--- src/app/components/product/newrelease/newrelease.component.ts
+++ src/app/components/product/newrelease/newrelease.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NewRelease } from 'src/app/interfaces/Product';
+import { CartProduct } from 'src/app/interfaces/Product';
 import { ProductService } from 'src/app/services/product.service';
+import { CartService } from 'src/app/services/cart.service';
 import { fadeInSlow } from 'src/app/utilities/animations';
 
 @Component({
@@ -12,14 +14,32 @@ import { fadeInSlow } from 'src/app/utilities/animations';
   ]
 })
 export class NewreleaseComponent {
+  cartProduct: CartProduct = {
+    name: '',
+    price: '',
+    imgSrc: '',
+    count: 0,
+  };
   newProduct: NewRelease = {
     name: '',
     price: '',
     imgSrc: '',
     premiere: '',
   }
+  isAddedToCart: boolean = false;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private cartService: CartService) {
     this.newProduct = this.productService.getNewRelease();
+  }
+
+  emitAddToCart() {
+    this.cartProduct.name = this.newProduct.name;
+    this.cartProduct.price = this.newProduct.price;
+    this.cartProduct.imgSrc = this.newProduct.imgSrc;
+    this.cartProduct.count = 1;
+
+    this.isAddedToCart = true;
+
+    this.cartService.addToCart(this.cartProduct);
   }
 }

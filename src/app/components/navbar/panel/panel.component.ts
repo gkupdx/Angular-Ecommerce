069463f4e146569ca20dd47932c94bc3fd243909
Grady Ku <gkupdx@gmail.com>
2023-04-27 15:21:18 -0700

    Integrate menu service

--- src/app/components/navbar/panel/panel.component.ts
+++ src/app/components/navbar/panel/panel.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { slideIn } from 'src/app/utilities/animations';
+import { Router } from '@angular/router';
+import { MenuService } from 'src/app/services/menu.service';
+import { slideDownSlow } from 'src/app/utilities/animations';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,11 +9,20 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css'],
   animations: [
-    slideIn,
+    slideDownSlow,
   ]
 })
 export class PanelComponent {
   cancelIcon = faXmark;
 
-  constructor() {}
+  constructor(private menuService: MenuService, private router: Router) {}
+
+  hideMenuPanel() {
+    this.menuService.toggleMenuPanel();
+  }
+
+  goToAccount() {
+    this.menuService.toggleMenuPanel();
+    this.router.navigate(['account']);
+  }
 }

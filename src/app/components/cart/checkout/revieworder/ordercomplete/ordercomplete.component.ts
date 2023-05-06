f938a052785e110dd1a56132027fa86db9d1745c
Grady Ku <gkupdx@gmail.com>
2023-05-06 16:50:50 -0700

    Add a mock order number to simulate a real transaction

--- src/app/components/cart/checkout/revieworder/ordercomplete/ordercomplete.component.ts
+++ src/app/components/cart/checkout/revieworder/ordercomplete/ordercomplete.component.ts
@@ -12,9 +12,14 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
   ]
 })
 export class OrdercompleteComponent {
+  orderNumber: number = 0;
   backArrow = faArrowLeft;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    let min = Math.ceil(10000);
+    let max = Math.floor(20000);
+    this.orderNumber = Math.floor(Math.random() * (max - min) + min);
+  }
 
   goToStore() {
     this.router.navigate(['store']);

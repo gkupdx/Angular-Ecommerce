a2ad34f020cf513879fd5c085c30b3e393f6a673
Grady Ku <gkupdx@gmail.com>
2023-05-10 21:03:52 -0700

    Update image src paths

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,54 +14,54 @@ export class ProductService {
     {
       name: '빨간 풍선 [Red Balloon]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F12%2F14%2Flandscape_red_balloo.2393912c.jpg&w=1366&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/12/14/landscape_red_balloo.2393912c.jpg',
     },
     {
       name: '야인시대 [Rustic Period]',
       price: '9.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F7%2F29%2Flandscape_the_wild_d.b29cedf3.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/7/29/landscape_the_wild_d.b29cedf3.jpg',
     },
     {
       name: '오 마이 비너스 [Oh My Venus]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F10%2F7%2Foh_my_venus_kr.e5cdfd33.jpg&w=1920&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/10/7/oh_my_venus_kr.e5cdfd33.jpg',
     },
     {
       name: '치얼업 [Cheer Up]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F9%2F30%2Flandscape_cheer_up_k.0e5da47c.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/9/30/landscape_cheer_up_k.0e5da47c.jpg',
     },
     {
       name: '일지매 [Iljimae]',
       price: '9.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2023%2F2%2F4%2Flandscape_il_ji_mae_.848d77b0.jpg&w=1366&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2023/2/4/landscape_il_ji_mae_.848d77b0.jpg',
     },
     {
       name: '모범택시 II [Taxi Driver 2]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2023%2F2%2F23%2Flandscape_Taxi_Drive.61f64a85.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2023/2/23/landscape_Taxi_Drive.61f64a85.jpg',
     },
     {
       name: '트레이서 [Tracer]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F8%2F3%2Flandscape_tracer_sea.6903b125.jpeg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/8/3/landscape_tracer_sea.6903b125.jpeg',
     },
     {
       name: '바람이 분다 [The Wind Blows]',
       price: '14.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F10%2F12%2Flandscape_the_wind_b.b01acd81.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/10/12/landscape_the_wind_b.b01acd81.jpg',
     },
     {
       name: '황금 가면 [Gold Mask]',
       price: '14.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F5%2F21%2Flandscape_gold_mask_.21f5bf6a.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/5/21/landscape_gold_mask_.21f5bf6a.jpg',
     },
     {
       name: '피아노 [Piano]',
       price: '4.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F7%2F27%2Flandscape_piano_kr.1a5d6566.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2022/7/27/landscape_piano_kr.1a5d6566.jpg',
     },
-  ]
+  ];
 
   constructor() { }
 
@@ -70,22 +70,22 @@ export class ProductService {
       {
         name: '빨간 풍선 [Red Balloon]',
         price: '19.99',
-        imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F12%2F14%2Flandscape_red_balloo.2393912c.jpg&w=1366&q=75',
+        imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/168x238/media-io/2022/12/14/portrait_red_balloon.78f2bdc6.jpg',
       },
       {
         name: '모범택시 II [Taxi Driver 2]',
         price: '19.99',
-        imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2023%2F2%2F23%2Flandscape_Taxi_Drive.61f64a85.jpg&w=384&q=75',
+        imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/168x238/media-io/2023/2/23/portrait_Taxi_Driver.58563f52.jpg',
       },
       {
         name: '일지매 [Iljimae]',
         price: '9.99',
-        imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2023%2F2%2F4%2Flandscape_il_ji_mae_.848d77b0.jpg&w=1366&q=75',
+        imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/168x238/media-io/2023/2/4/poster_il_ji_mae_kr.7bf3f2ab.jpg',
       },
       {
         name: '오 마이 비너스 [Oh My Venus]',
         price: '19.99',
-        imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2022%2F10%2F7%2Foh_my_venus_kr.e5cdfd33.jpg&w=1920&q=75',
+        imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/168x238/media-io/2022/10/7/oh_my_venus_poster_k.34dff2e8.jpg',
       },
     ];
   }
@@ -94,7 +94,7 @@ export class ProductService {
     return {
       name: '비밀의 여자 [Woman in a Veil]',
       price: '19.99',
-      imgSrc: 'https://beta.ondemandkorea.com/_next/image?url=https%3A%2F%2Fd2y2efdi5wgkcl.cloudfront.net%2Ffit-in%2F1600x900%2Fmedia-io%2F2023%2F3%2F14%2Flandscape_woman_in_a.627c5410.jpg&w=384&q=75',
+      imgSrc: 'https://d2y2efdi5wgkcl.cloudfront.net/fit-in/1024x0/filters:format(webp)/media-io/2023/3/14/landscape_woman_in_a.627c5410.jpg',
       premiere: '03/14/2023',
     }
   }

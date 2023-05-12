a450da1e402df1d6feb2cbab859791fdc67dd4aa
Grady Ku <gkupdx@gmail.com>
2023-05-12 16:05:03 -0700

    Remove duplicate check; update code

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
   isPasswordUpdated: boolean = false;
   isMembershipActive: boolean = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   getUserEmail(): string | null | undefined {
     const auth = getAuth();
@@ -31,36 +31,36 @@ export class AuthService {
 
   login(form: LoginForm) {
     if (this.isLoading) return;
-    
+
     this.isLoading = true;
 
     const auth = getAuth();
     signInWithEmailAndPassword(auth, form.email, form.password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      this.isLoading = false;
-      this.isAuthenticated = true;
-      this.router.navigate(['store']);
-    })
-    .catch((error) => {
-      this.isLoading = false;
-      this.isAuthenticated = false;
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    })
+      .then((userCredential) => {
+        const user = userCredential.user;
+        this.isLoading = false;
+        this.isAuthenticated = true;
+        this.router.navigate(['store']);
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        this.isAuthenticated = false;
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      })
   }
 
   resetPassword(form: PwdResetForm) {
     const auth = getAuth();
     sendPasswordResetEmail(auth, form.email)
-    .then(() => {
-      this.isLoading = false;
-    })
-    .catch((error) => {
-      this.isLoading = false;
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    })
+      .then(() => {
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      })
   }
 
   updatePassword(form: UpdatePassForm): boolean {
@@ -68,47 +68,40 @@ export class AuthService {
       this.isPasswordMatch = false;
       return false;
     }
-    
+
     const auth = getAuth();
     const user = auth.currentUser;
     const newPassword = form.newPassword;
 
     if (user) {
       updatePassword(user, newPassword)
-      .then(() => {
-        this.isPasswordUpdated = true;
-      }).catch((error) => {
-        console.log(error);
-      })
+        .then(() => {
+          this.isPasswordUpdated = true;
+        }).catch((error) => {
+          console.log(error);
+        })
     }
 
     return this.isPasswordUpdated;
   }
 
   register(form: RegForm) {
-    if (form.password !== form.passConfirm) {
-      this.isPasswordMatch = false;
-      return;
-    }
-
     if (this.isLoading) return;
     this.isLoading = true;
 
     const auth = getAuth();
     createUserWithEmailAndPassword(auth, form.email, form.password)
       .then((userCredential) => {
-        this.isAuthenticated = true;
         const user = userCredential.user;
+        this.isLoading = false;
+        this.isAuthenticated = true;
+        this.router.navigate(['store']);
       })
       .catch((error) => {
         this.isAuthenticated = false;
         const errorCode = error.code;
         const errorMessage = error.message;
       })
-      .finally(() => {
-        this.isLoading = false;
-        this.router.navigate(['store']);
-      });
   }
 
   reauthenticate() {
@@ -123,17 +116,17 @@ export class AuthService {
   }
 
   reactivation() {
-    this.isMembershipActive =true;
+    this.isMembershipActive = true;
   }
 
   logout() {
     const auth = getAuth();
     signOut(auth)
-    .then(() => {
-      this.isAuthenticated = false;
-    })
-    .catch((error) => {
-      console.log("Logout failed. Please try again.");
-    }).finally(() => this.router.navigate(['']));
+      .then(() => {
+        this.isAuthenticated = false;
+      })
+      .catch((error) => {
+        console.log("Logout failed. Please try again.");
+      }).finally(() => this.router.navigate(['']));
   }
 }

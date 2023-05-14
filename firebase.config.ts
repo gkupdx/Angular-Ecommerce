d6d4674d63442156e50a8a3c983da5db72d19885
Grady Ku <gkupdx@gmail.com>
2023-05-13 17:38:50 -0700

    Setup Firebase Realtime Database init

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,12 @@ export const firebaseConfig = {
   projectId: "angular-ecommerce-22bbe",
   storageBucket: "angular-ecommerce-22bbe.appspot.com",
   messagingSenderId: "306952987659",
-  appId: "1:306952987659:web:235a0f120a9f8b5018930e"
+  appId: "1:306952987659:web:235a0f120a9f8b5018930e",
+  databaseURL: "https://angular-ecommerce-22bbe-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Realtime Database
+export const database = getDatabase(app);

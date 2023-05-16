f20b916dcafde36c7b7e913cb963fd4feee8d65a
Grady Ku <gkupdx@gmail.com>
2023-05-15 19:30:40 -0700

    Delete uid action & reducer

--- src/app/state/uid.actions.ts
+++
@@ -1,3 +0,0 @@
-import { createAction } from "@ngrx/store";
-
-export const incrementCounter = createAction('[Register Component] Register User in Database');
--- src/app/state/uid.reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer, on } from "@ngrx/store";
-import { incrementCounter } from "./uid.actions";
-
-export const initialState = 0;
-
-export const uidReducer = createReducer(
-    initialState,
-    on(incrementCounter, (state) => state += 1),
-);

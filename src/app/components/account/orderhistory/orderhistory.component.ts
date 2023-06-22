f17337c3ef3b8a8d05b23e0940fbd205a0590013
Grady Ku <gkupdx@gmail.com>
2023-06-21 18:19:21 -0700

    Add functionality to view an individual order's contents

--- src/app/components/account/orderhistory/orderhistory.component.ts
+++ src/app/components/account/orderhistory/orderhistory.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { DatabaseService } from 'src/app/services/database.service';
 import { slideIn } from 'src/app/utilities/animations';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-orderhistory',
@@ -13,12 +13,25 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
   ]
 })
 export class OrderhistoryComponent {
+  isBtnClicked: boolean[] = [];
   orderKeys: string[] = [];
   orderProducts: Map<string, string[]> = new Map();
   arrowLeft = faArrowLeft;
+  caretDown = faCaretDown;
 
   constructor(private location: Location, private dbService: DatabaseService) {
     this.dbService.getOrderHistory(this.orderKeys, this.orderProducts);
+    for (let i = 0; i < this.orderKeys.length; i++) {
+      this.isBtnClicked[i] = false;
+    }
+  }
+
+  viewOrderContents(index: number) {
+    this.isBtnClicked[index] = !this.isBtnClicked[index];
+  }
+
+  isOrderReq(index: number): boolean {
+    return this.isBtnClicked[index];
   }
 
   goToPrev() {
